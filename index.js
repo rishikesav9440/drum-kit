d96cb7f8ac482a098cb375b3eb55f4c15d282b68
Rishi Kesav <121454665+rishikesav9440@users.noreply.github.com>
2023-01-24 00:10:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,76 @@
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+function onMouseClick(){
+
+for(var i = 0 ; i<numberOfDrumButtons ; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+
+        var buttonHtmlKey = this.innerHTML;
+        soundPlay(buttonHtmlKey);
+        transitionA(buttonHtmlKey);
+    })
+    
+    
+}
+}
+function onKeyClick(){
+
+    addEventListener("keydown",function(event){
+            soundPlay(event.key);
+            transitionA(event.key);
+        })
+        
+        
+    
+    }
+
+
+function soundPlay(key){
+    switch (key) {
+        case "w":
+            var audio1 = new Audio('sounds/tom-1.mp3');
+            audio1.play();
+            break;
+        case "a":
+            var audio2 = new Audio('sounds/tom-2.mp3');
+            audio2.play();
+            break;
+        case "s":
+            var audio3 = new Audio('sounds/tom-3.mp3');
+            audio3.play();
+            break;
+        case "d":
+            var audio4 = new Audio('sounds/tom-4.mp3');
+            audio4.play();
+            break;    
+        case "j":
+            var audio5 = new Audio('sounds/snare.mp3');
+            audio5.play();
+            break;
+        case "k":
+            var audio6 = new Audio('sounds/crash.mp3');
+            audio6.play();
+            break;
+        case "l":
+            var audio7 = new Audio('sounds/kick-bass.mp3');
+            audio7.play();
+            break;        
+        default:
+            console.log("Some Error");
+            break;
+    }
+}
+function transitionA(buttonHtmlKey){
+var buttonClickNow = document.querySelector("."+buttonHtmlKey);
+buttonClickNow.classList.add("pressed");
+buttonClickNow.classList.remove("drum");
+buttonClickNow.classList.add("drum2");
+setTimeout(function(){
+    buttonClickNow.classList.remove("pressed");
+    buttonClickNow.classList.remove("drum2");
+buttonClickNow.classList.add("drum");
+},100);
+
+
+}
+onMouseClick();
+onKeyClick();
